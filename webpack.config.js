56ff2179287475064c198729976b9ee8575adfe6
Joshua Moore <joshua.moore@leudla.net>
2020-08-31 13:28:56 -0700

    need to build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/chat/",
     },
     module: {
         rules: [
